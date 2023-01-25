5792f39a9bf73447c8709561bb3ac9c3016ebdb3
AVIRAL GUPTA <guptaaviral59@gmail.com>
2023-01-26 02:31:18 +0530

    sanity

---
+++ dapp/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'dapp',
+
+  projectId: 'wk56bmxd',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
